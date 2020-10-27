94f66cf6bfe9c6335d5e846d212029ac796a0820
Annette Lien <s202633@student.dtu.dk>
2020-10-27 13:50:45 +0000

    Cookiecutter Data Science project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project work for the DTU course 27410 Computer-aided cell factory design',
+    author='Group 12',
+    license='MIT',
+)
